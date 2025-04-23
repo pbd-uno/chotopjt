78cf54855fc637d191567ab824968ce0f09e2ef1
yuki.uno <yuki.uno@persol.co.jp>
2025-04-23 14:26:01 +0900

    add news ISR

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -6,7 +6,7 @@ type Props = {
   }
 }
 
-export const revalidate = 0;
+export const revalidate = 60;
 
 export default async function Page( {params}: Props ) {
   const data = await getNewsDetail(params.slug)
